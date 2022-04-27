1b8324753431502d6c6d55631acb55cde9ebf3c3
Shawn Edwards <shawn.edwards@.com>
2022-04-27 20:53:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "https://github.com/stick152/terraform-aws-s3-bucket"
+  source  = "stick152/terraform-aws-s3-bucket"
   version = "3.1.1"
   # insert required variables here
 }
